971018ea66ef370e08cacd088bb0380851b50fb6
Jarno Sandrin <jarnosandrin@gmail.com>
2022-05-30 08:14:47 +0200

    update

--- ENOB/main.py
+++ ENOB/main.py
@@ -1,22 +1,9 @@
-from readfile import readfile
-import pandas as pd
-import matplotlib.pyplot as plt
-import glob
-import os
+import enob
 
-dataframe = readfile("sin.txt")
 
-fig = plt.figure(figsize=(11, 6), dpi=80)
+sin = enob.enob("sin.txt")
+sin.fft_method()
 
-plt.title('ADCH')
-plt.ylabel('ADC code')
-plt.xlabel('Sample #')
-plt.grid(True)
-plt.plot(dataframe["H"],
-color="dodgerblue",  markeredgecolor = 'black',
-    marker = ".", markersize = 8, linestyle = 'dotted')
 
-fig.tight_layout()
 
-plt.show()
 
